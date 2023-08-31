7e0bd61e8487ed76c9665353e88dfa53ffa99a84
Nagib Moura Suaid <nagibmoura@gmail.com>
2023-08-31 19:56:16 -0300

    WIP LightsOut

---
+++ Projeto Baralho/Assets/Scripts/LightsOut/LightsOut.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightsOut : MonoBehaviour
+{
+    public List<LightsOutCell> cells;
+    public int rows;
+    public int columns;
+    public static LightsOut instance;
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("Multiple instances of LightsOut!");
+            Destroy(this);
+        }
+        if (cells.Count != rows * columns)
+        {
+            Debug.LogError("LightsOut não foi configurado corretamente!");
+        }
+        for (int i = 0; i < cells.Count; i++)
+        {
+            cells[i].index = i;
+        }
+    }
+
+    public int GetIndex(int x, int y)
+    {
+        return y * columns + x;
+    }
+    public bool CellExists(int x, int y)
+    {
+        if (x >= columns || x < 0)
+        {
+            return false;
+        }
+        if (y >= rows || y < 0)
+        {
+            return false;
+        }
+        return true;
+    }
+    public void TryFlipCell(int x, int y)
+    {
+        if (CellExists(x, y))
+        {
+            cells[GetIndex(x, y)].Flip();
+        }
+    }
+
+    public void OnCellClicked(int index)
+    {
+        int x = index % columns;
+        int y = index / columns;
+
+        TryFlipCell(x, y);
+        TryFlipCell(x + 1, y);
+        TryFlipCell(x - 1, y);
+        TryFlipCell(x, y + 1);
+        TryFlipCell(x, y - 1);
+    }
+}
---
+++ Projeto Baralho/Assets/Scripts/LightsOut/LightsOutCell.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightsOutCell : MonoBehaviour
+{
+    public bool isOn;
+    public int index;
+    public Image image;
+
+    void UpdateSprite()
+    {
+        if (image == null)
+        {
+            image = GetComponent<Image>();
+        }
+        if (isOn)
+        {
+            image.color = Color.white;
+        }
+        else
+        {
+            image.color = Color.black;
+        }
+    }
+    private void Start()
+    {
+        UpdateSprite();
+    }
+    public void Flip()
+    {
+        isOn = !isOn;
+        UpdateSprite();
+    }
+    public void OnClick()
+    {
+        LightsOut.instance.OnCellClicked(index);
+    }
+}

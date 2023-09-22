f3e174e448544c5b991faea948c00e7bc254a29f
Nagib Moura Suaid <nagibmoura@gmail.com>
2023-09-22 18:45:43 -0300

    Symbol Lock Solve

--- Projeto Baralho/Assets/Scripts/SymbolLock/SymbolLock.cs
+++ Projeto Baralho/Assets/Scripts/SymbolLock/SymbolLock.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class SymbolLock : MonoBehaviour
@@ -8,6 +9,8 @@ public class SymbolLock : MonoBehaviour
     public List<Sprite> allSprites;
     public List<Sprite> correctSequence;
     public List<Image> images;
+
+    public UnityEvent onSolve;
 
     public void CheckSolved()
     {
@@ -18,7 +21,19 @@ public class SymbolLock : MonoBehaviour
                 return;
             }
         }
-        Debug.Log("RESOLVEU!!");
+        foreach (var image in images)
+        {
+            foreach (Transform child in image.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+        Invoke( nameof(OnSolve), 1f ); 
+    }
+
+    void OnSolve()
+    {
+        onSolve.Invoke();
     }
 
     public void MoveUp(int imageIndex)

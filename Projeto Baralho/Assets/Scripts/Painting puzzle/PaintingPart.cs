86177e73b82339e944a5bd822dfd19f87be2018c
AlOzorio <arthurlozorio@gmail.com>
2023-09-21 23:14:01 -0300

    Remove debugs

--- Projeto Baralho/Assets/Scripts/Painting puzzle/PaintingPart.cs
+++ Projeto Baralho/Assets/Scripts/Painting puzzle/PaintingPart.cs
@@ -13,30 +13,28 @@ public class PaintingPart : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public Pencil rightPencil;
     [HideInInspector]
     public RectTransform rectTransform;
-     // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
- 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
         // Mouse entered the image
         isMouseOver = true;
-        Debug.Log("Mouse entered the image.");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         // Mouse exited the image
         isMouseOver = false;
-        Debug.Log("Mouse exited the image.");
     }
 
 }

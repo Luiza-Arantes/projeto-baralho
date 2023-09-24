2810846a802e532a18341304c8ccf9cb15e17dee
Nagib Moura Suaid <nagibmoura@gmail.com>
2023-09-24 19:37:21 -0300

    ItemReceiver

---
+++ Projeto Baralho/Assets/Scripts/Inventory/ItemReceiver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemReceiver : MonoBehaviour, IDropHandler
+{
+    [SerializeField] private InventorySO inventorySO;
+    [SerializeField] private string itemName;
+    [SerializeField] private UnityEvent onReceive;
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            InventoryItemSO item = inventorySO.itens.Find(x => x.Icon == eventData.pointerDrag.GetComponent<Image>().sprite);
+
+            if (item.Name == itemName)
+            {
+                onReceive.Invoke();
+            }
+        }
+    }
+}

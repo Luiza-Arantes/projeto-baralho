62ea6cbdfacfd55afb532b088889e0c6fc8a70bb
Nagib Moura Suaid <nagibmoura@gmail.com>
2023-11-09 21:14:31 -0300

    Progression Scripts

---
+++ Projeto Baralho/Assets/Scripts/Progression/Progression.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progression : MonoBehaviour
+
+{
+    static Dictionary<string, int> events = new Dictionary<string, int>() { };
+
+    public static int GetEventCounter(string eventId)
+    {
+        if (events.ContainsKey(eventId))
+        {
+            return events[eventId];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    public static void IncrementCounter(string eventId)
+    {
+        if (!events.ContainsKey(eventId))
+        {
+            events[eventId] = 1;
+        }
+        else
+        {
+            events[eventId] += 1;
+        }
+    }
+
+}
---
+++ Projeto Baralho/Assets/Scripts/Progression/ProgressionSpriteSwitcher.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressionSpriteSwitcher : MonoBehaviour
+{
+	public string eventId;
+	public List<Sprite> sprites;
+	public Sprite defaultSprite;
+	public Image image;
+
+	void OnEnable()
+	{
+		UpdateSprite();
+	}
+
+	void UpdateSprite()
+	{
+		int value = Progression.GetEventCounter(eventId);
+		if(value < sprites.Count)
+		{
+			image.sprite = sprites[value];
+		}
+		else
+		{
+			image.sprite = defaultSprite;
+		}
+	}
+}
